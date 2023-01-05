ca0d444877b9aefc7016d47f5ae9d44d196d6e56
Faizahmedsf <121433435+Faizahmedsf@users.noreply.github.com>
2023-01-05 18:05:45 +0530

    second commit

--- index.js
+++ index.js
@@ -3,70 +3,83 @@ console.log('Index.js');
 // table class all table methods
 // datasource class from where the data is coming
 
-const jsonformdata = () => {
-    loadtable = document.getElementById('loadtable');
-    loadData = document.getElementById('loadData');
+getallinstance = () => {
+    let data = new dataSource();
+    let table = new Tableclass(data)
 
-    clearBtn = document.querySelectorAll('.clear-btn');
+}
 
-    loadData.innerText = 'Refresh Data'
-    console.log('json form data() clicked');
+class main {
+    constructor(){
+         let data = new dataSource();
+        //  console.log(data.getdata())
 
-    class main {
+         let table = new Tableclass(data)
+        //  console.log(table.view())
+         table.view()
+
+    }
+}
+
+class dataSource {
+    getdata() {
+        // console.log('getdata')
+        return [
+            {
+                "id": 1,
+                "first_name": "faiz ",
+                "middle_name": "ahmed",
+                "last_name": "choudhary",
+                "email": "faiz@mail.com",
+                "phone_number": "8878-86876",
+                "role": "trainee",
+                "address": "Mumbai"
+            },
+            {
+                "id": 2,
+                "first_name": "Zain",
+                "middle_name": "New",
+                "last_name": "Malik",
+                "email": "malik@mail.com",
+                "phone_number": "12321-9887",
+                "role": "Band",
+                "address": "Delhi"
+            },
+            {
+                "id": 3,
+                "first_name": "React",
+                "middle_name": "JS",
+                "last_name": "Native",
+                "email": "react@mail.com",
+                "phone_number": "0987-564321",
+                "role": "Developer",
+                "address": "USA"
+            }
+        ]
+    }
+}
+
+class Tableclass {
+
+    constructor(data) {
+        this.data = data
+        // const globaldata = data.getdata()    
+        // this.view()
 
     }
 
-    class dataSource {
-        getdata() {
-            // console.log('getdata')
-            return [
-                {
-                    "id": 1,
-                    "first_name": "faiz ",
-                    "middle_name": "ahmed",
-                    "last_name": "choudhary",
-                    "email": "faiz@mail.com",
-                    "phone_number": "8878-86876",
-                    "role": "trainee",
-                    "address": "Mumbai"
-                },
-                {
-                    "id": 2,
-                    "first_name": "Zain",
-                    "middle_name": "New",
-                    "last_name": "Malik",
-                    "email": "malik@mail.com",
-                    "phone_number": "12321-9887",
-                    "role": "Band",
-                    "address": "Delhi"
-                },
-                {
-                    "id": 3,
-                    "first_name": "React",
-                    "middle_name": "JS",
-                    "last_name": "Native",
-                    "email": "react@mail.com",
-                    "phone_number": "0987-564321",
-                    "role": "Developer",
-                    "address": "USA"
-                }
-            ]
-        }
-    }
+    view() {
+        let data = new dataSource();
+        let table = new Tableclass(data);
 
-    class Tableclass {
-
-        constructor(data) {
-            this.data = data
-            // const globaldata = data.getdata()
-        }
-
-        view() {
+        loadtable = document.getElementById('loadtable');
+ 
+        console.log('j')
             loadtable.innerHTML = ''
             // console.log('getdata')
             // console.log(data.getdata())
             const tableData = data.getdata()
-
+            console.log(tableData)
             // now let create a table header
             var tableheader = [];
             for (let i = 0; i < tableData.length; i++) {
@@ -126,9 +139,9 @@ const jsonformdata = () => {
                 editButton.classList.add('edit-btn', 'btn-info')
                 editButton.innerText = 'Edit';
                 let trid = 'tbodyrow-' + i
-                editButton.addEventListener("click", table.edit(trid , tableheader));
+                editButton.addEventListener("click", table.edit(trid, tableheader));
                 let editcell = tbodyrow.insertCell(-1)
-                editcell.setAttribute('id' , 'savecleartd')
+                editcell.setAttribute('id', 'savecleartd')
                 editcell.append(editButton)
                 tbodyrow.append(editcell)
 
@@ -145,7 +158,7 @@ const jsonformdata = () => {
                 savebutton.setAttribute('id', 'save-button-' + i);
                 savebutton.innerText = 'Save';
                 savebutton.classList.add('btn-primary', 'save');
-                savebutton.addEventListener("click", table.save(trid , tableheader));
+                savebutton.addEventListener("click", table.save(trid, tableheader));
                 savebutton.style.display = 'none';
 
                 // clear button
@@ -153,126 +166,128 @@ const jsonformdata = () => {
                 clearbutton.setAttribute('id', 'clear-button-' + i);
                 clearbutton.innerText = 'Clear';
                 clearbutton.classList.add('btn-info', 'clear', 'ml-2');
-                clearbutton.addEventListener("click", table.clear(trid , tableheader, tableData));
+                clearbutton.addEventListener("click", table.clear(trid, tableheader, tableData));
                 clearbutton.style.display = 'none';
 
                 // let tablelastcell = tbodyrow.insertCell(-1);
                 editcell.append(savebutton)
-                editcell.append( clearbutton);
+                editcell.append(clearbutton);
 
             }
-
-        }
-
-        // edit
-        edit(id , tableheader) {
-            return function () {
-                // console.log(id)
-                let editelem = document.getElementById(id)
-                editelem.style.backgroundColor = '#E2C4B8';
-
-                for (let i = 0; i < tableheader.length; i++) {
-                    let getcell = editelem.querySelectorAll('.tcell')[i]
-                    getcell.setAttribute('contenteditable' , 'true')
-                    // console.log(getcell)    
-                }
-
-                // lets toggle buttons
-                for (let j = 0; j < editelem.querySelector('#savecleartd').children.length; j++) {  
-                    // console.log("editelem.querySelector('#savecleartd').children[i]")
-                    // console.log(j)
-                    console.log(editelem.querySelector('#savecleartd').children[j])
-
-                    if (editelem.querySelector('#savecleartd').children[j].style.display === 'none') {
-                        editelem.querySelector('#savecleartd').children[j].style.display = 'block'
-                    } else {
-                        editelem.querySelector('#savecleartd').children[j].style.display = 'none'
-
-                    }
-                }                
-            }
-        }
-
-        // delete
-        delete(id) {
-            return function () {
-                console.log(id)
-                let delelem = document.getElementById(id)
-                delelem.remove()   
-            }
-        }
-
-        // clear
-        clear(id , tableheader, tableData) {
-            return function () {
-             console.log('clear')
-                 let clearelem = document.getElementById(id);
-                 let clearelemindex = document.getElementById(id).rowIndex;
-                 console.log(clearelemindex)
-                 
-                 clearelem.style.backgroundColor = '#fff';
-                 
-                 for (let i = 0; i < tableData.length; i++) {
-                     
-                     for (let j = 0; j < tableheader.length; j++) {
-                         let content = tableData[clearelemindex - 1][tableheader[j]];
-                          clearelem.querySelectorAll('.tcell')[j].innerText = content;
-                    }
-                    
-                 }
-
-                   // lets toggle buttons
-                   for (let j = 0; j < clearelem.querySelector('#savecleartd').children.length; j++) {  
-                    // console.log("editelem.querySelector('#savecleartd').children[i]")
-                    // console.log(j)
-                    console.log(clearelem.querySelector('#savecleartd').children[j])
-
-                    if (clearelem.querySelector('#savecleartd').children[j].style.display === 'none') {
-                        clearelem.querySelector('#savecleartd').children[j].style.display = 'block'
-                    } else {
-                        clearelem.querySelector('#savecleartd').children[j].style.display = 'none'
-
-                    }
-                }
-             
-            } 
-         }
-
-
-        // save
-        save(id , tableheader) {
-           return function () {
+        
+    }
+    // edit
+    edit(id, tableheader) {
+        return function () {
             // console.log(id)
-                let saveelem = document.getElementById(id)
-                saveelem.style.backgroundColor = '#fff';
+            let editelem = document.getElementById(id)
+            editelem.style.backgroundColor = '#E2C4B8';
 
-                for (let i = 0; i < tableheader.length; i++) {
-                    let getcell = saveelem.querySelectorAll('.tcell')[i]
-                    getcell.setAttribute('contenteditable' , 'false')
-                    // console.log(getcell)    
+            for (let i = 0; i < tableheader.length; i++) {
+                let getcell = editelem.querySelectorAll('.tcell')[i]
+                getcell.setAttribute('contenteditable', 'true')
+                // console.log(getcell)    
+            }
+
+            // lets toggle buttons
+            for (let j = 0; j < editelem.querySelector('#savecleartd').children.length; j++) {
+                // console.log("editelem.querySelector('#savecleartd').children[i]")
+                // console.log(j)
+                console.log(editelem.querySelector('#savecleartd').children[j])
+
+                if (editelem.querySelector('#savecleartd').children[j].style.display === 'none') {
+                    editelem.querySelector('#savecleartd').children[j].style.display = 'block'
+                } else {
+                    editelem.querySelector('#savecleartd').children[j].style.display = 'none'
+
                 }
-
-                
-                   // lets toggle buttons
-                   for (let j = 0; j < saveelem.querySelector('#savecleartd').children.length; j++) {  
-                    // console.log("editelem.querySelector('#savecleartd').children[i]")
-                    // console.log(j)
-                    console.log(saveelem.querySelector('#savecleartd').children[j])
-
-                    if (saveelem.querySelector('#savecleartd').children[j].style.display === 'none') {
-                        saveelem.querySelector('#savecleartd').children[j].style.display = 'block'
-                    } else {
-                        saveelem.querySelector('#savecleartd').children[j].style.display = 'none'
-
-                    }
-           } 
+            }
         }
     }
+
+    // delete
+    delete(id) {
+        return function () {
+            console.log(id)
+            let delelem = document.getElementById(id)
+            delelem.remove()
+        }
     }
-    let data = new dataSource();
-    // console.log(data.getdata())
 
-    let table = new Tableclass(data)
-    table.view()
+    // clear
+    clear(id, tableheader, tableData) {
+        return function () {
+            console.log('clear')
+            let clearelem = document.getElementById(id);
+            let clearelemindex = document.getElementById(id).rowIndex;
+            console.log(clearelemindex)
 
+            clearelem.style.backgroundColor = '#fff';
+
+            for (let i = 0; i < tableData.length; i++) {
+
+                for (let j = 0; j < tableheader.length; j++) {
+                    let content = tableData[clearelemindex - 1][tableheader[j]];
+                    clearelem.querySelectorAll('.tcell')[j].innerText = content;
+                }
+
+            }
+
+            // lets toggle buttons
+            for (let j = 0; j < clearelem.querySelector('#savecleartd').children.length; j++) {
+                // console.log("editelem.querySelector('#savecleartd').children[i]")
+                // console.log(j)
+                console.log(clearelem.querySelector('#savecleartd').children[j])
+
+                if (clearelem.querySelector('#savecleartd').children[j].style.display === 'none') {
+                    clearelem.querySelector('#savecleartd').children[j].style.display = 'block'
+                } else {
+                    clearelem.querySelector('#savecleartd').children[j].style.display = 'none'
+
+                }
+            }
+
+        }
+    }
+
+    // save
+    save(id, tableheader) {
+        return function () {
+            // console.log(id)
+            let saveelem = document.getElementById(id)
+            saveelem.style.backgroundColor = '#fff';
+
+            for (let i = 0; i < tableheader.length; i++) {
+                let getcell = saveelem.querySelectorAll('.tcell')[i]
+                getcell.setAttribute('contenteditable', 'false')
+                // console.log(getcell)    
+            }
+
+
+            // lets toggle buttons
+            for (let j = 0; j < saveelem.querySelector('#savecleartd').children.length; j++) {
+                // console.log("editelem.querySelector('#savecleartd').children[i]")
+                // console.log(j)
+                console.log(saveelem.querySelector('#savecleartd').children[j])
+
+                if (saveelem.querySelector('#savecleartd').children[j].style.display === 'none') {
+                    saveelem.querySelector('#savecleartd').children[j].style.display = 'block'
+                } else {
+                    saveelem.querySelector('#savecleartd').children[j].style.display = 'none'
+
+                }
+            }
+        }
+    }
+
+}
+
+const jsonformdata = () => {
+    loadtable = document.getElementById('loadtable');
+    loadData = document.getElementById('loadData');
+
+    loadData.innerText = 'Refresh Data'
+    console.log('json form data() clicked');
+
+    let mainclass = new main();
 }
